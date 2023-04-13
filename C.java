de0c661b8322443e7279245f47a53964c500372c
pankajanadure <pankajanadure@gmail.com>
2023-04-13 14:28:36 +0530

    Modified C.java

--- C.java
+++ C.java
@@ -2,6 +2,13 @@ public class C
 {
 	public void display()
 	{
-		System.out.println("Just for demo purpose");
+		int i=1;
+		while(true)
+		{
+			System.out.println("Just for demo purpose");
+			if(i==10)
+				break;
+			i++;
+		}
 	}
 }
